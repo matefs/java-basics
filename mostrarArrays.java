8a64ccd4e746f0065e2467975a95c579649306a2
Mateus Schverz <matefs8569@gmail.com>
2025-02-21 23:28:39 -0300

    Create mostrarArrays.java

---
+++ mostrarArrays.java
@@ -0,0 +1,13 @@
+import java.text.MessageFormat;
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+         int[] idades = {2,43,41};
+         System.out.println(Arrays.toString(idades));
+    }
+}
+
+
+
+
